50b4b489b30f95f68e356ce1a297cab8c6ad1089
stailzgit <77897064+stailzgit@users.noreply.github.com>
2022-07-25 02:37:13 +0300

    port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
+const PORT = process.env.PORT || 3000;
 // const MONGODB = "mongodb+srv://admin:coopercodes@apolloserversetup.n9ghj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const MONGODB =
   "mongodb+srv://stailzsanchez:ghjcnjnfr@cluster0.cslchur.mongodb.net/?retryWrites=true&w=majority";
@@ -13,12 +14,7 @@ const server = new ApolloServer({
   resolvers,
 });
 
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
-  .then(() => {
-    console.log("MongoDB Connected");
-    return server.listen({ port: process.env.PORT || 5000 });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  });
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log("MongoDB Connected");
+  server.listen(() => console.log(`Server started on port - ${PORT}`));
+});
