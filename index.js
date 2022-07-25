5a8cfb4a26f069715c1775deb6bc2f297dae88e1
stailzgit <77897064+stailzgit@users.noreply.github.com>
2022-07-25 17:37:06 +0300

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 // const MONGODB = "mongodb+srv://admin:coopercodes@apolloserversetup.n9ghj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const MONGODB =
   "mongodb+srv://stailzsanchez:ghjcnjnfr@cluster0.cslchur.mongodb.net/?retryWrites=true&w=majority";
@@ -12,6 +12,15 @@ const MONGODB =
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // csrfPrevention: true,
+  // cache: "bounded",
+  // cors: {
+  //   origin: [
+  //     "https://www.your-app.example",
+  //     "https://studio.apollographql.com",
+  //     "https://gql-auth-server.herokuapp.com/",
+  //   ],
+  // },
 });
 
 mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
